30764579452ef25ebe4341af1192f30370da620a
Siafeson <siafeson@Mac-mini-de-Siafeson.local>
2024-02-23 11:48:43 -0700

    Filter

--- app/src/main/java/movil/siafeson/simgolp/adapters/LocationListAdapter.kt
+++ app/src/main/java/movil/siafeson/simgolp/adapters/LocationListAdapter.kt
@@ -1,6 +1,7 @@
 package movil.siafeson.simgolp.adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,6 +15,8 @@ class LocationListAdapter(
     private var locations: List<LocationData>
 ) : BaseAdapter()
      {
+         private val originalLocationDataList: List<LocationData> = ArrayList(locations)
+
          override fun getCount(): Int {
              return locations.size
          }
@@ -38,10 +41,32 @@ class LocationListAdapter(
             binding.listLocationName.text = this.locations[position].predio ?: "N/A"
             binding.listLocationPosition.text =  "${this.locations[position].distancia} Km. | Dir: ${this.locations[position].orientacion}"
             binding.listLocationHa.text = this.locations[position].superficie.toString()
-        return binding.root
-    }
+            return binding.root
+        }
          fun updateData(newLocationDataList: List<LocationData>) {
              locations = newLocationDataList
              notifyDataSetChanged()
          }
-}
+         // Función para realizar el filtrado
+         fun filter(query: String) {
+             //Filtrando en base a la lista original
+             val filteredList = originalLocationDataList.filter { locationData ->
+                 // Aplica lógica de filtrado aquí
+                 Log.d("Entrando a filter", "$locationData")
+                 locationData.predio.contains(query, ignoreCase = true)
+              }
+             Log.d("filteredList", "$filteredList")
+
+             // Actualiza la lista filtrada y notifica el cambio
+             locations = filteredList
+             notifyDataSetChanged()
+             Log.d("LocationListAdapter", "Lista después del filtrado: $locations")
+
+         }
+         // Funcion para restablecer la lista original
+         fun restoreOriginalList(){
+             locations = ArrayList(originalLocationDataList)
+             notifyDataSetChanged()
+         }
+
+    }
